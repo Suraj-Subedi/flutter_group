843df4886f58a6d1d74a323e319b3f56c0760a33
Suraj <soorajsubedi1227@gmail.com>
2023-07-07 15:07:19 +0545

    register, check admin, get User

--- auth/register.php
+++ auth/register.php
@@ -2,18 +2,46 @@
 
 //function for registering a user
 include_once '../helper_functions/AuthenicationFunction.php';
+include_once '../helper_functions/DatabaseConfig.php';
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
-    if (isset($_POST['fullname']) && isset($_POST['email']) && isset($_POST['password'])) {
-        $result =  register($_POST['fullname'], $_POST['email'], $_POST['password']);
+    $email = $_POST['email'];
+    $fullname = $_POST['fullname'];
+    $password = $_POST['password'];
+
+    if (isset($email) && isset($fullname) && isset($password)) {
+
+        $sql = "SELECT * FROM users WHERE email = '$email'";
+
+        $users = mysqli_query($con, $sql);
+        $usersCount = mysqli_num_rows($users);
+
+        if ($usersCount > 0) {
+            echo json_encode([
+                "status" => 409,
+                "message" => "User already exists"
+            ]);
+            return;
+        }
+
+        $result =  register($fullname, $email, $password);
 
         if ($result) {
-            echo "User registered successfully";
+            echo json_encode([
+                "status" => 200,
+                "message" => "User registered successfully"
+            ]);
         } else {
-            echo "User registration failed";
+            echo json_encode([
+                "status" => 500,
+                "message" => "Something went wrong"
+            ]);
         }
     } else {
-        echo "Please fill all the fields";
+        echo json_encode([
+            "status" => 400,
+            "message" => "Please fill all the fields"
+        ]);
     }
 }
